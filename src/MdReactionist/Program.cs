b350f0676f572dfaa9c65942149bbe05ba145a83
Marcin Galiński <marcinggalinski@gmail.com>
2023-05-10 00:20:34 +0200

    move code to method + only react to mention, not reply

--- src/MdReactionist/Program.cs
+++ src/MdReactionist/Program.cs
@@ -6,13 +6,15 @@ namespace MdReactionist;
 
 public class Program
 {
+    private static BotOptions _options = new BotOptions();
+    
     public static async Task Main()
     {
         var token = Environment.GetEnvironmentVariable("MD_BOT_TOKEN");
         if (string.IsNullOrEmpty(token))
             throw new ArgumentException("Valid token is required for bot to work.");
 
-        var options = GetBotOptions();
+        _options = GetBotOptions();
         var client  = new DiscordSocketClient(
             new DiscordSocketConfig
             {
@@ -22,20 +24,7 @@ public class Program
         await client.LoginAsync(TokenType.Bot, token);
         await client.StartAsync();
 
-        client.MessageReceived += async messageParam =>
-        {
-            if (messageParam is not SocketUserMessage message)
-                return;
-
-            var isUserMentioned = message.MentionedUsers.Select(x => x.Id).Intersect(options.TriggeringUserIds).Any();
-            var isRoleMentioned = message.MentionedRoles.Select(x => x.Id).Intersect(options.TriggeringRoleIds).Any();
-            var isContainingSubstring = options.TriggeringSubstrings.Any(x => message.Content.Contains(x, StringComparison.InvariantCultureIgnoreCase));
-
-            if (isUserMentioned || isRoleMentioned || isContainingSubstring)
-            {
-                await message.AddReactionAsync(Emote.Parse(options.EmoteId));
-            }
-        };
+        client.MessageReceived += AddReaction;
 
         // block this task until the program is closed
         await Task.Delay(-1);
@@ -48,5 +37,21 @@ public class Program
         var botOptions = config.GetSection("BotOptions").Get<BotOptions>();
 
         return botOptions ?? new BotOptions();
+    }
+
+    private static async Task AddReaction(SocketMessage message)
+    {
+        if (message is not SocketUserMessage msg)
+            return;
+
+        // manual check in message content to overcome the fact that reply is treated the same as mention in SocketMessage
+        var isUserMentioned = _options.TriggeringUserIds.Select(x => $"<@{x}>").Any(x => msg.Content.Contains(x, StringComparison.InvariantCultureIgnoreCase));
+        var isRoleMentioned = _options.TriggeringRoleIds.Select(x => $"<@&{x}>").Any(x => msg.Content.Contains(x, StringComparison.InvariantCultureIgnoreCase));
+        var isContainingSubstring = _options.TriggeringSubstrings.Any(x => msg.Content.Contains(x, StringComparison.InvariantCultureIgnoreCase));
+
+        if (isUserMentioned || isRoleMentioned || isContainingSubstring)
+        {
+            await msg.AddReactionAsync(Emote.Parse(_options.EmoteId));
+        }
     }
 }
