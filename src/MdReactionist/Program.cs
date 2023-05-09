3a85600c41d4052e18b88b8928842ce51fd0b1cd
Marcin Galiński <marcinggalinski@gmail.com>
2023-05-09 22:18:17 +0200

    simple bot

--- src/MdReactionist/Program.cs
+++ src/MdReactionist/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace MdReactionist;
+
+public class Program
+{
+    private static readonly DiscordSocketClient _client = new DiscordSocketClient();
+    
+    public static async Task Main()
+    {
+        var token = Environment.GetEnvironmentVariable("MD_BOT_TOKEN");
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // block this task until the program is closed
+        await Task.Delay(-1);
+    }
+}
