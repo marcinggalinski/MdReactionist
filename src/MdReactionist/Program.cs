4a3d243122998097d28aff379f4d1a233f83b019
Marcin Galiński <marcinggalinski@gmail.com>
2023-05-09 22:42:05 +0200

    react with specified reaction to any message mentioning specified user or role (values hardcoded for now)

--- src/MdReactionist/Program.cs
+++ src/MdReactionist/Program.cs
@@ -5,13 +5,29 @@ namespace MdReactionist;
 
 public class Program
 {
+    private const ulong UserId = 829016888846581812;
+    private const ulong RoleId = 1098699786850422904;
+    private const string EmoteId = "<:md:1025013446682619966>";
+        
     private static readonly DiscordSocketClient _client = new DiscordSocketClient();
+    private static readonly IEmote _mdEmote = Emote.Parse(EmoteId);
     
     public static async Task Main()
     {
         var token = Environment.GetEnvironmentVariable("MD_BOT_TOKEN");
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
+
+        _client.MessageReceived += async messageParam =>
+        {
+            if (messageParam is not SocketUserMessage message)
+                return;
+
+            if (message.MentionedUsers.Any(x => x.Id == UserId) || message.MentionedRoles.Any(x => x.Id == RoleId))
+            {
+                await message.AddReactionAsync(_mdEmote);
+            }
+        };
 
         // block this task until the program is closed
         await Task.Delay(-1);
