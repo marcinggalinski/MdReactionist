51df5c85ad1a02e222c214f7c2d77be181af1e79
Marcin Galiński <marcinggalinski@gmail.com>
2023-05-10 12:28:35 +0200

    add correcting

--- src/MdReactionist/BotOptions.cs
+++ src/MdReactionist/BotOptions.cs
@@ -3,6 +3,7 @@
 public class BotOptions
 {
     public EmoteReactions[] EmoteReactions { get; set; } = Array.Empty<EmoteReactions>();
+    public Correction[] Corrections { get; set; } = Array.Empty<Correction>();
 }
     
 public class EmoteReactions
@@ -12,4 +13,10 @@ public class EmoteReactions
     public ulong[] TriggeringUserIds { get; set; } = Array.Empty<ulong>();
     public ulong[] TriggeringRoleIds { get; set; } = Array.Empty<ulong>();
     public string[] TriggeringSubstrings { get; set; } = Array.Empty<string>();
+}
+
+public class Correction
+{
+    public string? StringToCorrect { get; set; }
+    public string? CorrectedString { get; set; }
 }
--- src/MdReactionist/HostedServices/DiscordBotHostedService.cs
+++ src/MdReactionist/HostedServices/DiscordBotHostedService.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
 
 namespace MdReactionist.HostedServices;
 
@@ -18,12 +19,14 @@ public class DiscordBotHostedService : IHostedService, IDisposable
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _client.MessageReceived += AddReaction;
+        _client.MessageReceived += Correct;
         return Task.CompletedTask;
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
         _client.MessageReceived -= AddReaction;
+        _client.MessageReceived -= Correct;
 
         await _client.StopAsync();
         await _client.LogoutAsync();
@@ -53,6 +56,36 @@ public class DiscordBotHostedService : IHostedService, IDisposable
                 else if (options.Emoji is not null)
                     await msg.AddReactionAsync(new Emoji(options.Emoji));
             }
+        }
+    }
+
+    private async Task Correct(SocketMessage message)
+    {
+        if (message is not SocketUserMessage msg)
+            return;
+
+        foreach (var options in _options.Corrections)
+        {
+            if (string.IsNullOrEmpty(options.StringToCorrect) || string.IsNullOrEmpty(options.StringToCorrect))
+                continue;
+            
+            var index = msg.Content.IndexOf(options.StringToCorrect, StringComparison.InvariantCulture);
+            if (index == -1)
+                continue;
+
+            var correctionStart = 0;
+            var correctionEnd = msg.Content.Length;
+
+            var prevWhitespace = msg.Content[..index].LastOrDefault(char.IsWhiteSpace);
+            if (prevWhitespace != default(char))
+                correctionStart = msg.Content[..index].LastIndexOf(prevWhitespace) + 1;
+
+            var nextWhitespace = msg.Content[index..].FirstOrDefault(char.IsWhiteSpace);
+            if (nextWhitespace != default(char))
+                correctionEnd = index + msg.Content[index..].IndexOf(nextWhitespace);
+
+            var correction = $"*{msg.Content[correctionStart..correctionEnd].Replace(options.StringToCorrect, options.CorrectedString)}";
+            await msg.ReplyAsync(correction);
         }
     }
 }
